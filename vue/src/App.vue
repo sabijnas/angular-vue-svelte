cbf2bee0719b2ff23e52dd38e4bdea463e65b7a8
sabijnas <sabinah97@hotmail.com>
2026-04-30 10:46:01 +0200

    api

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,11 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+
+type Dog = { id: string; name: string; description: string };
+
+const data = ref<Dog[]>([]);
+
+onMounted(async () => {
+  const res = await fetch("http://localhost:3000/dogs");
+  data.value = await res.json();
+});
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>DOGS</h1>
+
+  <div v-for="dog in data" :key="dog.id">
+    <button>🩷</button>
+    <h4>{{ dog.name }}</h4>
+    <p>{{ dog.description }}</p>
+  </div>
 </template>
 
 <style scoped></style>
